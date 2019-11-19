5e0def20e613a71dfefad7c967a5d2f99314f081
rapkat10 <aminrapkat@gmail.com>
2019-11-19 01:23:45 -0700

    Homework

---
+++ W6D2/Graph_Nodes_BFS.rb
@@ -0,0 +1,39 @@
+require 'byebug'
+class GraphNode
+  attr_accessor :value, :neighbors
+
+  def initialize(value)
+    @value = value
+    @neighbors = []
+  end
+
+  def bfs(start_node, target)
+    queue = [start_node]
+    visited = []
+    # debugger
+    until queue.empty?
+        node = queue.shift
+        return node if node.value == target
+        unless visited.include?(node)
+            neighbor = node.neighbors.shift
+            queue.push(neighbor)
+            visited.push(neighbor)
+        end
+    end
+    nil
+  end
+end
+
+
+a = GraphNode.new('a')
+b = GraphNode.new('b')
+c = GraphNode.new('c')
+d = GraphNode.new('d')
+e = GraphNode.new('e')
+f = GraphNode.new('f')
+a.neighbors = [b, c, e]
+c.neighbors = [b, d]
+e.neighbors = [a]
+f.neighbors = [e]
+p a.bfs(a, "b")
+p a.bfs(a, "f")
