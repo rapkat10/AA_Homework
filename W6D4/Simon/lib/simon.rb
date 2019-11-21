89defc7ff5e112e4e12c01b68c5f58eb2c631c0f
rapkat10 <aminrapkat@gmail.com>
2019-11-21 00:05:35 -0700

    Simon HW ~passed specs~ but not fully playable

--- W6D4/Simon/lib/simon.rb
+++ W6D4/Simon/lib/simon.rb
@@ -32,7 +32,7 @@ class Simon
   end
 
   def require_sequence
-    puts "pick the shown color"
+    puts "pick the shown color!"
     input = gets.chomp
     if seq[0] != input
       game_over = true
