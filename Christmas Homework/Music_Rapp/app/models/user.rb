a6a19533d8d6133098c23cdc534feec9bc8507c5
rapkat10 <aminrapkat@gmail.com>
2019-12-25 23:56:13 -0700

    hw

--- Christmas Homework/Music_Rapp/app/models/user.rb
+++ Christmas Homework/Music_Rapp/app/models/user.rb
@@ -3,6 +3,20 @@ class User < ApplicationRecord
     validates :email, :password_digest, :session_token
     validates :password, length: {minumum: 6, allow_nil: true}
 
+    after_initialize :ensure_session_token!
+
+
+
+
+
     
+ 
+
+
+    def ensure_session_token!
+        
+    end
+
+
 
 end
