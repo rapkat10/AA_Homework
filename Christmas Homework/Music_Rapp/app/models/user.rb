bf5a113186c75b103a359432bc0cb73ab4f287e2
rapkat10 <aminrapkat@gmail.com>
2019-12-27 23:50:11 -0700

    hw

--- Christmas Homework/Music_Rapp/app/models/user.rb
+++ Christmas Homework/Music_Rapp/app/models/user.rb
@@ -3,11 +3,12 @@ class User < ApplicationRecord
     validates :email, :password_digest, :session_token
     validates :password, length: {minumum: 6, allow_nil: true}
 
+    
     after_initialize :ensure_session_token!
 
     def self.find_by_credentials(email, password)
         user = user.find_by(email: email)
-        
+
     end
 
     def password=(password)
