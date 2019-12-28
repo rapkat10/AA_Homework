a0303463104a72b9fa2247c9ca713e0fc9535f4e
rapkat10 <aminrapkat@gmail.com>
2019-12-27 23:51:08 -0700

    hw

--- Christmas Homework/Music_Rapp/app/models/user.rb
+++ Christmas Homework/Music_Rapp/app/models/user.rb
@@ -14,7 +14,7 @@ class User < ApplicationRecord
 
     def password=(password)
         @password = password
-        
+        self.password_digest = BCrypt::Password.create(password)
     end
 
     def is_password?(password)
