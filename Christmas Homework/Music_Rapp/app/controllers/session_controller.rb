cedb0309379fe08378baa42f212647c5b42d72ed
rapkat10 <aminrapkat@gmail.com>
2019-12-29 21:35:52 -0700

    music_app hw

--- Christmas Homework/Music_Rapp/app/controllers/session_controller.rb
+++ Christmas Homework/Music_Rapp/app/controllers/session_controller.rb
@@ -1,2 +1,22 @@
 class SessionController < ApplicationController
+    def new
+        render :new
+    end
+
+    def create
+        user = User.find_by_credentials(params[:user][:email],params[:user][:password])
+        if user.nil?
+            flash.now[:errors] = ["Wrong username and/or password!"]
+            render :new
+        else
+            login_user!(user)
+            redirect_to root_url
+        end
+    end
+
+    def destroy
+        current_user.reset_session_token!
+        session[:session_token] = nil
+        redirect_to new_session_url
+    end
 end
--- Christmas Homework/Music_Rapp/app/controllers/user_controller.rb
+++ Christmas Homework/Music_Rapp/app/controllers/user_controller.rb
@@ -1,2 +1,26 @@
 class UserController < ApplicationController
+    def show
+    render :show
+    end
+
+    def new
+        @user = User.new
+        render :new
+    end
+
+    def create
+        @user = User.new(user_params)
+
+        if @user.save
+            login_user!(@user)
+            redirect_to root_url
+        else
+            flash.now[:errors] = @user.errors.full_messages
+            render :new
+        end
+    end
+
+    def user_params
+        params.require(:user).permit(:email, :password)
+    end
 end
