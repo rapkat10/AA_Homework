bd34385d0e43ea6bd288b993923af34f221bdf59
rapkat10 <aminrapkat@gmail.com>
2020-01-08 17:52:16 -0700

    W7D3 hw

---
+++ Week 7/W7D3/lru.rb
@@ -0,0 +1,54 @@
+
+class LRUCache
+
+  def initialize(n)
+    @size = n
+    @array = []
+  end
+
+  def count
+    @array.size
+  end
+
+  # def add(el)
+  #   @array.push(el)
+  #   @array.uniq!
+  # end
+  def add(el)
+    if @array.include?(el)
+      @array.delete(el)
+      @array << el
+    elsif count >= @size
+      @array.shift
+      @array << el
+    else
+      @array << el
+    end
+  end
+
+  def show
+    p @array
+  end
+
+  
+end
+
+johnny_cache = LRUCache.new(4)
+
+johnny_cache.add("I walk the line")
+johnny_cache.add(5)
+
+johnny_cache.count # => returns 2
+
+johnny_cache.add([1,2,3])
+johnny_cache.add(5)
+johnny_cache.add(-5)
+johnny_cache.add({a: 1, b: 2, c: 3})
+johnny_cache.add([1,2,3,4])
+johnny_cache.add("I walk the line")
+johnny_cache.add(:ring_of_fire)
+johnny_cache.add("I walk the line")
+johnny_cache.add({a: 1, b: 2, c: 3})
+
+
+johnny_cache.show # => prints [[1, 2, 3, 4], :ring_of_fire, "I walk the line", {:a=>1, :b=>2, :c=>3}]
