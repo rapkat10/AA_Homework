b57f1f683e7cd055a49c2ef1672f5fe240641315
rapkat10 <aminrapkat@gmail.com>
2020-01-16 16:38:47 -0700

    meta_corgis hw

--- Week 8/W8D4/MetaCorgis/meta_corgis.rb
+++ Week 8/W8D4/MetaCorgis/meta_corgis.rb
@@ -110,12 +110,21 @@ class MetaCorgiSnacks
   end
 
   def method_missing(name, *args)
-    # Your code goes here...
-    
+
+    info = @snack_box.send("get_#{name}_info", @box_id)
+    tastiness = @snack_box.send("get_#{name}_tastiness", @box_id)
+    result = "#{name}: #{info}: #{tastiness} "
+    tastiness > 30 ? "* #{result}" : result
   end
 
 
   def self.define_snack(name)
-    # Your code goes here...
+
+    define_method(name) do
+      info = @snack_box.send("get_#{name}_info", @box_id)
+      tastiness = @snack_box.send("get_#{name}_tastiness", @box_id)
+      result = "#{name.capitalize}: #{info}: #{tastiness} "
+      tastiness > 30 ? "* #{result}" : result
+    end
   end
 end
